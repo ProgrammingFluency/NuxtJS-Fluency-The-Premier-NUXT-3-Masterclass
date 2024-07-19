e5a0a7b11a4db7e234ecd758993bdcd1d6e269e2
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-20 01:39:03 +0300

    NavBar comp sol

---
+++ 1-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Views [vue components]/NavBar-component-solution/components/NavBar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <nav class="navbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  href="/Services">Services</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  href="/Pricing">Pricing</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link"  href="/Contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    .navbar {
+        background-color: #333;
+        padding: 10px
+    }
+
+    .navbar-nav {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        display: flex;
+    }
+
+    .nav-item {
+        margin-left: 15px
+    }
+
+    .nav-link {
+        color: #fff;
+        text-decoration: none;
+        font-weight: bold
+    }
+</style>
