98e95283d07e058337b14c5dcdfc10d4ac7475b5
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-20 20:11:18 +0300

    greeting exercise

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route Parameters]/Coding Exercise/greeting-exercise-solution/pages/greeting/[name].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Good morning {{ name }}</h1>
+    </div>
+</template>
+
+<script setup>
+    const route = useRoute()
+    const {name} = route.params
+</script>
---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route Parameters]/Coding Exercise/greeting-exercise-solution/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <NuxtLink :to="`/greeting/${name}`">See your greeting</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+    let name
+    if(process.client) {
+        name = prompt('Please enter your name')
+    }
+</script>
+
