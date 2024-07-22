950b3fe2121b7f3fbdc8022a27f2c9202de948a1
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-22 17:55:53 +0300

    pages lecture before deleting app.vue file

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Pages]/Lecture src code/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <Info/>
+    <h1>Heyyy</h1>
+    <NuxtPage/>
+  </div>
+</template>
---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Pages]/Lecture src code/components/Info.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1>Nuxt JS app</h1>
+        <p>Full stack framework</p>
+    </div>
+</template>
