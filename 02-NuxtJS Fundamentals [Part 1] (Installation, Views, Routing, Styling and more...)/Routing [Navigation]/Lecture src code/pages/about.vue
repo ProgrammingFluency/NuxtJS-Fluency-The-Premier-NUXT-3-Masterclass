f7a6ea89523309152fd1b0e90d7fbbc3eda60a3c
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-23 17:10:24 +0300

    Routing: navigation lecture src code

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Navigation]/Lecture src code/pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>About page</h1>
+        <NuxtLink to="/">
+            Home page (NuxtLink)
+        </Nuxtlink>
+        <br><br>
+        <a href="/">Home page (Normal link)</a>
+        <br><br>
+        <NuxtLink to="/about">
+            About page (NuxtLink)
+        </Nuxtlink>
+        <br><br>
+        <a href="/about">About page (Normal link)</a>
+        <br><br>
+        
+    </div>
+</template>
---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Navigation]/Lecture src code/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Home page</h1>
+        <NuxtLink to="/about">
+            About page (NuxtLink)
+        </Nuxtlink>
+        <br><br>
+        <a href="/about">About page (Normal link)</a>
+        <br><br>
+        <NuxtLink to="/">
+            Home page (NuxtLink)
+        </Nuxtlink>
+        <br><br>
+        <a href="/">Home page (Normal link)</a>
+    </div>
+</template>
+
