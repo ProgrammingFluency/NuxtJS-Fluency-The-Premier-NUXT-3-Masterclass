8a20d71e5726ff5fde1158a4663f4ef9f9f84c3d
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-24 16:18:04 +0300

    route middleware-file middleware

--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route middleware (Part 1)]/pages/about.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route middleware (Part 1)]/pages/about.vue
@@ -1,11 +1,7 @@
 <script setup>
     definePageMeta({
         middleware: [
-            function(to, from) {
-                console.log(from)
-                console.log(to)
-                console.log('This code will run before navigating to the about route')
-            }
+            'middleware-example'
         ]
     })
 </script>
