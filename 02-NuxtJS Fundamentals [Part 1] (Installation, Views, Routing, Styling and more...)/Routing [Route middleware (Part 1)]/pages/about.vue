5ec2b6cf32f19363a50ff7d0d6c3f666ca8cedfb
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-24 16:08:45 +0300

    route middlewware -  inline middleware

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route middleware (Part 1)]/pages/about.vue
@@ -0,0 +1,18 @@
+<script setup>
+    definePageMeta({
+        middleware: [
+            function(to, from) {
+                console.log(from)
+                console.log(to)
+                console.log('This code will run before navigating to the about route')
+            }
+        ]
+    })
+</script>
+
+<template>
+    <div>
+        <h1>About page</h1>
+    </div>
+</template>
+
