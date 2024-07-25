00d697563722edbb30b1fd22a711fe5934ff6acd
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-25 17:23:48 +0300

    route middleware part 2

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route middleware (Part 2)]/pages/about.vue
@@ -0,0 +1,18 @@
+<script setup>
+    definePageMeta({
+        middleware: [
+            function(to, from) {
+                console.log('inline middleware')
+            },
+            'middleware-example'
+        ]
+    })
+</script>
+
+<template>
+    <div>
+        <h1>About page</h1>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
+
---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Routing [Route middleware (Part 2)]/pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <h1>Home page</h1>
+        <NuxtLink to="/about">About page</NuxtLink>
+    </div>
+</template>
