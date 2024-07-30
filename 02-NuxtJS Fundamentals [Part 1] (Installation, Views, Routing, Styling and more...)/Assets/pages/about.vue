e5b1fa0c3bd88f8cc275849c96c0df59f8be720d
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-30 21:30:30 +0300

    assets directory

--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Assets/pages/about.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Assets/pages/about.vue
@@ -1,5 +1,10 @@
 <template>
     <div>
         <h1>About page</h1>
+        <NuxtLink to="/">
+            Home page
+        </NuxtLink>
+
     </div>
 </template>
+
--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Assets/pages/index.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Assets/pages/index.vue
@@ -1,6 +1,18 @@
 <template>
     <div>
         <h1>Home page</h1>
+        <NuxtLink to="/about">
+            About page
+        </NuxtLink>
+        <br>
+        <img src="/images/nuxt.png" alt="">
+        <img src="/assets/images/nuxt.png" alt="">
     </div>
 </template>
 
+<style>
+    img {
+        width: 80px;
+        height: 80px;
+    }
+</style>
