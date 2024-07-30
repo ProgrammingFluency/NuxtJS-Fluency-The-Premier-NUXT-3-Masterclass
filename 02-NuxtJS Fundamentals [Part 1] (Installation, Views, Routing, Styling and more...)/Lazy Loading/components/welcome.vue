982b22588f04ffe95882f309d356ab83985711fc
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-30 20:44:30 +0300

    lazy loading

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Lazy Loading/components/welcome.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Welcome to Nuxt JS</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Lazy Loading/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <LazyWelcome v-if="show"/>
+        <button @click="show = !show">Show welcome message</button>
+    </div>
+</template>
+
+<script setup>
+    const show = ref(false)
+</script>
