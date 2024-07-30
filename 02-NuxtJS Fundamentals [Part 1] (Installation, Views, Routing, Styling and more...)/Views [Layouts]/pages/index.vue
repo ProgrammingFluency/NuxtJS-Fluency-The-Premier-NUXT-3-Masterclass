e5d8a286914b2e5afe12a533d66de9cdf861c17a
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-30 20:19:29 +0300

    setting layout dynamically using setPageLayout

--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Views [Layouts]/pages/index.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Views [Layouts]/pages/index.vue
@@ -1,8 +1,17 @@
+<script setup>
+    const enableCustomLayout = () => {
+        setPageLayout('custom')
+    }
+</script>
+
 <template>
     <div>
         <h1>Home page</h1>
         <nuxtLink to="/about">
             About page
         </nuxtLink>
+        <br>
+        <br>
+        <button @click="enableCustomLayout">Change layout</button>
     </div>
 </template>
