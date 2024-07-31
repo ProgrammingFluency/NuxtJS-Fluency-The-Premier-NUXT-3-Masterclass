7e5bd926ab8e0fbf827e96c7b364113d4edc9715
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-07-31 18:06:42 +0300

    adding tailwind css

--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Styling/nuxt.config.ts
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Styling/nuxt.config.ts
@@ -1,8 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  compatibilityDate: '2024-07-31',
   css: [
     '~/assets/styles/style.css',
     'animate.css'
+  ],
+  modules: [
+    "@nuxtjs/tailwindcss"
   ]
 })
--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Styling/pages/index.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Styling/pages/index.vue
@@ -4,7 +4,7 @@
 <template>
     <div>
         <h1>Home page</h1>
-        <h1 class="animate__animated animate__bounce">Hello Nuxt JS</h1>
+        <h1 class="text-5xl font-extrabold dark:text-white">Heading 1</h1>
     </div>
 </template>
 <style scoped>
