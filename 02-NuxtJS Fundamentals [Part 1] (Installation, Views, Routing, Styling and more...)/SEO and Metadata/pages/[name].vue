7192d0edd72fee888f4397d222181e374dad2f6a
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-03 00:23:17 +0300

    using a dynamic title

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/SEO and Metadata/pages/[name].vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <Head>
+            <Title>
+                {{ $route.params.name }}
+            </Title>
+        </Head>
+    </div>
+</template>
