1a6bc15254d0d7269b22e9a53c60c115e02d7cbe
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-03 00:09:22 +0300

    useHead()

--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/SEO and Metadata/nuxt.config.ts
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/SEO and Metadata/nuxt.config.ts
@@ -2,23 +2,4 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=2',
-      title: 'Nuxt app',
-      meta: [
-        {
-          name: 'description',
-          content: 'Nuxt course'
-        }
-      ],
-      link: [
-        {
-          rel: 'stylesheet',
-          href: '~/assets/styles/styles.css'
-        }
-      ]
-    }
-  }
 })
--- 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/SEO and Metadata/pages/about.vue
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/SEO and Metadata/pages/about.vue
@@ -1,5 +1,20 @@
+<script setup>
+    useHead({
+        title: 'about page',
+        meta: [
+           {
+            name: 'description',
+            content: 'this is about page'
+           }
+        ]
+    })
+</script>
+
 <template>
     <div>
         <h1>About page</h1>
+        <NuxtLink to="/">
+            Home page
+        </NuxtLink>
     </div>
 </template>
