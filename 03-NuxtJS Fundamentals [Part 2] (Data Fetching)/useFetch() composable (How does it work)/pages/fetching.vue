fef52cb4318a302e661fe4b35b8e57618f23c762
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-05 22:18:36 +0300

    How to use useFetch()

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/useFetch() composable (How does it work)/pages/fetching.vue
@@ -0,0 +1,10 @@
+<script setup>
+    const {data: products} = await useFetch('https://fakestoreapi.com/products')
+    console.log(products.value)
+</script>
+
+<template>
+    <div>
+        {{ products }}
+    </div>
+</template>
