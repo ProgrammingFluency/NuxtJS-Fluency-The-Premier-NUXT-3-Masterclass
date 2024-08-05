7b654985ff24a469ad0a5d4ddf7c9ebf1dd6e63a
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-05 21:30:12 +0300

    Transitions: page transitions

---
+++ 02-NuxtJS Fundamentals [Part 1] (Installation, Views, Routing, Styling and more...)/Transitions/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>Home page</h1>
+        <NuxtLink to="/about">About page</NuxtLink>
+    </div>
+</template>
+<style>
+    .page-enter-active,
+    .page-leave-active {
+        transition: all 0.4s;
+    }
+
+    .page-enter-from,
+    .page-leave-to {
+        opacity: 0;
+        filter: blur(1rem)
+    }
+</style>
