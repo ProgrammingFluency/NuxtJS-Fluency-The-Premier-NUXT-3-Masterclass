90dcc97cf080ee8300120f369c8ceb9717a06c09
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-09 16:18:55 +0300

    adding dynamic URLs

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/02- Adding Dynamic URLS/pages/fetching.vue
@@ -0,0 +1,25 @@
+<script setup>
+    const id = ref(1)
+    const {data: product, refresh} = await useFetch(() => {
+        return `https://fakestoreapi.com/products/${id.value}`
+    })
+    console.log(product.value)
+    const fetchProduct = async () => {
+        id.value++
+        console.log('ID 👇👇')
+        console.log(id.value)
+        await refresh()
+        console.log('Product 👇👇')
+        console.log(product.value)
+    }
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="fetchProduct">
+            Display a new product
+        </button>
+    </div>
+</template>
