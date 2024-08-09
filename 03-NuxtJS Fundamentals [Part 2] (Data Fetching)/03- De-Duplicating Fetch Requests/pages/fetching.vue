a5bba0181055bb1a5be18f52ef3ecea2d4a29f2b
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-09 22:23:23 +0300

    de-duplication of requests

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/03- De-Duplicating Fetch Requests/pages/fetching.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const {data: product, refresh} = await useFetch('https://fakestoreapi.com/products/1', {
+        key: 'product'
+    })
+
+    const fetchData = () => {
+        const interval = setInterval(async () => {
+            await refresh()
+            console.log(product.value)
+        },1)
+
+        setTimeout(() => {
+            clearInterval(interval)
+        },2000)
+    }
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="fetchData">Get products</button>
+    </div>
+</template>
