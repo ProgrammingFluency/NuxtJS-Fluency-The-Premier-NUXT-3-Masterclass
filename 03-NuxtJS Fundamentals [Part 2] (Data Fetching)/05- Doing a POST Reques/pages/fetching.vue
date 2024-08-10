9ee535f61f53c1e2bc59192d868bf8c209b8379e
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-10 03:55:34 +0300

    doing a POST request

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/05- Doing a POST Reques/pages/fetching.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const {data: posts} = await useFetch('posts', {
+        baseURL: 'https://jsonplaceholder.typicode.com',
+        method: 'post',
+        body: {
+            title: 'hey',
+            body: 'hello world',
+            userID: 1
+        }
+    })
+    
+</script>
+
+<template>
+    <div>
+        {{ posts }}
+    </div>
+</template>
