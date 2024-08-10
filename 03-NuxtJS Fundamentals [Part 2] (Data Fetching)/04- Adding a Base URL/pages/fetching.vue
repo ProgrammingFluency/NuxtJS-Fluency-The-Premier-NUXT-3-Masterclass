9d329cc4afbb46ceb4992097a52571b73fc589d4
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-10 03:40:10 +0300

    adding a base URL

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/04- Adding a Base URL/pages/fetching.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const {data: products} = await useFetch('products/categories', {
+        baseURL: 'https://fakestoreapi.com'
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ products }}
+        <br><br>
+    </div>
+</template>
