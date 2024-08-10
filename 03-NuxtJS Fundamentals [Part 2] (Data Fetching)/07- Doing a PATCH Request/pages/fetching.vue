6ab78f719d41c108c8320303677392fb026ecbb2
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-10 20:22:03 +0300

    doing a patch request

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/07- Doing a PATCH Request/pages/fetching.vue
@@ -0,0 +1,16 @@
+<script setup>
+    const {data: posts} = await useFetch('posts/1', {
+        baseURL: 'https://jsonplaceholder.typicode.com',
+        method: 'patch',
+        body: {
+            title: 'patched',
+        }
+    })
+    
+</script>
+
+<template>
+    <div>
+        {{ posts }}
+    </div>
+</template>
