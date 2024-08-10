f02997515b1c0cf5c22a9ff6e61cf6c7488831df
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-10 20:01:33 +0300

    doing a put request

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/06- Doing a PUT Request)/pages/fetching.vue
@@ -0,0 +1,18 @@
+<script setup>
+    const {data: posts} = await useFetch('posts/1', {
+        baseURL: 'https://jsonplaceholder.typicode.com',
+        method: 'put',
+        body: {
+            title: 'updated',
+            body: 'updating post with id 1',
+            userID: 1
+        }
+    })
+    
+</script>
+
+<template>
+    <div>
+        {{ posts }}
+    </div>
+</template>
