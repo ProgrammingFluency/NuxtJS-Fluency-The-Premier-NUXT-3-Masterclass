f15ae43e17023820b0f2cb77babe3181f873b62f
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-16 02:07:36 +0300

    adding query or params

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/08- Adding query or params/pages/fetching.vue
@@ -0,0 +1,20 @@
+<script setup>
+    const apiKey = 'VNOOE1JXQ49E2EOZ'
+    const {data: stocks} = await useFetch('query', {
+        baseURL: 'https://www.alphavantage.co',
+        params: {
+            function: 'TIME_SERIES_INTRADAY',
+            symbol: 'IBM',
+            interval: '5min',
+            apikey: apiKey
+        },
+        timeout: 5000
+    })
+    
+</script>
+
+<template>
+    <div>
+        {{ stocks }}
+    </div>
+</template>
