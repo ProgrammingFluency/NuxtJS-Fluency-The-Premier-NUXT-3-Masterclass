8bd35b92332d635da93f86676282099cdcdbbd2c
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-17 00:38:45 +0300

    avoid fetching the data from the server

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/09- Avoid fetching data on the server/pages/fetching.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const {data: product} = await useFetch('https://fakestoreapi.com/products/1', {
+        server: false
+    })
+
+    console.log(product.value)
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+    </div>
+</template>
