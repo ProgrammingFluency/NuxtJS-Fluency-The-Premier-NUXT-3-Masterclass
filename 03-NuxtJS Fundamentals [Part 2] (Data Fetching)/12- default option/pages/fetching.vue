8fd9147503c7fdaf2a7cee3fa81c49b579654832
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-17 19:54:44 +0300

    default option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/12- default option/pages/fetching.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+        immediate: false,
+        default: (product) => {
+            product = 'No data available'
+            return product
+        }
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="execute">Display data</button>
+    </div>
+</template>
