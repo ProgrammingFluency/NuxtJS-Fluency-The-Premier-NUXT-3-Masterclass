394484911bb80fa138883a4b0fbda48460a8d350
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-17 19:25:40 +0300

    immediate option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/11- immediate option/pages/fetching.vue
@@ -0,0 +1,15 @@
+<script setup>
+    const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+        immediate: false
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="execute">Display data</button>
+    </div>
+</template>
