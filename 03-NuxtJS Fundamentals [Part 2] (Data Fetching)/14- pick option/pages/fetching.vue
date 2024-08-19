70fab965a7090fe961e4eebc9b944117ce812197
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-19 15:32:42 +0300

    pick option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/14- pick option/pages/fetching.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+        pick: ['title']
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+    </div>
+</template>
