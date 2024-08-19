7e22108f303470a4b7e07f2f699c9c63cf1e388d
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-19 15:23:05 +0300

    transform option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/13- transform option/pages/fetching.vue
@@ -0,0 +1,19 @@
+<script setup>
+    const {data: product, execute} = await useFetch('https://fakestoreapi.com/products/1', {
+        transform: (product) => {
+            return {
+                ...product,
+                title: 'Mobile'
+            }
+        }
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+    </div>
+</template>
