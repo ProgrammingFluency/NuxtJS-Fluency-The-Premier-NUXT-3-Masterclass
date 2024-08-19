b8c7cb71506a1686ec7dec82edf80a04a510b053
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-19 15:59:45 +0300

    watch option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/15- watch option/pages/fetching.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const id = ref(1)
+    const {data: product} = await useFetch(() => {
+        return `https://fakestoreapi.com/products/1`
+    }, {
+        watch: [id],
+        transform: (product) => {
+            console.log(product)
+            console.log(id)
+            return product
+        }
+    })
+
+    const incrementID = () => {
+        id.value++
+    }
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="incrementID">Re fetch</button>
+    </div>
+</template>
