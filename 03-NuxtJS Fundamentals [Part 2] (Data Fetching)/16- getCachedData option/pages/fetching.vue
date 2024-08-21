e38cf4756040ea4f5a99db78d1b404a159e51c36
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-21 16:41:10 +0300

    getCachedData option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/16- getCachedData option/pages/fetching.vue
@@ -0,0 +1,20 @@
+<script setup>
+    const nuxtApp = useNuxtApp()
+    const {data: product} = await useFetch(() => {
+        return `https://fakestoreapi.com/products/1`
+    }, {
+        getCachedData: key => {
+            return nuxtApp.payload.data[key] || nuxtApp.static.data[key]
+        }
+    })
+
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/16- getCachedData option/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h1>Home page</h1>
+        <NuxtLink to="/fetching">
+            Fetching page
+        </NuxtLink>
+    </div>
+</template>
