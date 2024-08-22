88d8d4196957bf035bd9ab7aafa8db3eacb56947
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-22 19:37:34 +0300

    deep option

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/17- deep option/pages/fetching.vue
@@ -0,0 +1,20 @@
+<script setup>
+    const {data: product} = await useFetch(() => {
+        return `https://fakestoreapi.com/products/1`
+    }, {
+        deep: false,
+    })
+
+    const changeRating = () => {
+        product.value.rating.rate = 'any'
+    }
+    
+</script>
+
+<template>
+    <div>
+        {{ product }}
+        <br><br>
+        <button @click="changeRating">Change rating</button>
+    </div>
+</template>
