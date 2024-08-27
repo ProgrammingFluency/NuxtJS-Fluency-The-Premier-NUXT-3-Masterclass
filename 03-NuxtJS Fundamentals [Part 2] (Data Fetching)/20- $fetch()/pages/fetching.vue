d15d2c2c23e19a7842e758a60405e75cb771b6f9
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-08-28 00:59:18 +0300

    $fetch

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/20- $fetch()/pages/fetching.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const toDo = await $fetch('https://jsonplaceholder.typicode.com/todos/1')
+
+</script>
+
+<template>
+    <div>
+        {{ toDo }}
+        <br><br>
+    </div>
+</template>
