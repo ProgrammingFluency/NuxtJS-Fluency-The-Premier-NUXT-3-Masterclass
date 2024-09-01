5c73ee51081be1454abd4d932d848e5761a103bd
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-09-01 20:55:46 +0300

    useAsyncData

---
+++ 03-NuxtJS Fundamentals [Part 2] (Data Fetching)/21-  useAsyncData/pages/fetching.vue
@@ -0,0 +1,26 @@
+<script setup>
+    const { data } = await useAsyncData('productsAndTodo', async () => {
+        const [products, categories, todos] = await Promise.all([
+            $fetch('https://fakestoreapi.com/products'),
+            $fetch('https://fakestoreapi.com/products/categories'),
+            $fetch('https://jsonplaceholder.typicode.com/todos')
+        ])
+
+        return{ products, categories, todos }
+    })
+
+    console.log(data)
+    const products = data.value.products
+    const categories = data.value.categories
+    const todos = data.value.todos
+
+</script>
+
+<template>
+    <div>
+        <p>{{ products }}</p>
+        <p>{{ categories }}</p>
+        <p>{{ todos }}</p>
+        <br><br>
+    </div>
+</template>
