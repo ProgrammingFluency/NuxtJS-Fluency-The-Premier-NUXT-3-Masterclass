fc64ef052497a41beeeadb2811d085fc6f484942
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-09-13 23:15:56 +0300

    shared state

---
+++ 04-NuxtJS Fundamentals [Part 3] (State Management)/02- Shared State/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="counter++">Increment</button>
+        <br><br>
+        <NuxtLink to="/about">About page</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+const counter = useState('counterState',() => 1)
+
+</script>
