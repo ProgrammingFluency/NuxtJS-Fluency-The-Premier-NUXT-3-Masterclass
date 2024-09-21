badce8ddacd1f033467e21a2a01886ea64dfffbc
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-09-21 17:19:26 +0300

    shared state as a composable

---
+++ 04-NuxtJS Fundamentals [Part 3] (State Management)/03- Creating a shared State Composable/pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="counter++">Increment</button>
+        <br><br>
+        <NuxtLink to="/">Home page</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+const counter = useCounter()
+
+</script>
---
+++ 04-NuxtJS Fundamentals [Part 3] (State Management)/03- Creating a shared State Composable/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Counter: {{ counter }}</h1>
+        <button @click="counter++">Increment</button>
+        <br><br>
+        <NuxtLink to="/about">About page</NuxtLink>
+    </div>
+</template>
+
+<script setup>
+
+
+const counter = useCounter()
+
+</script>
