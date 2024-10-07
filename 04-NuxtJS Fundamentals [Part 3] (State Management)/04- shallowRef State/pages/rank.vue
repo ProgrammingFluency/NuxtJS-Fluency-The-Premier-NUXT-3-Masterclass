22858a44c3e0fdcdfebc27d2739d1a2b9220e49c
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-10-07 21:44:31 +0300

    shallowRef state

---
+++ 04-NuxtJS Fundamentals [Part 3] (State Management)/04- shallowRef State/pages/rank.vue
@@ -0,0 +1,17 @@
+<script setup>
+    const rank = useState('rank', () => {
+        return shallowRef({
+            num: 3
+        })
+    })
+
+    const changeRank = () => {
+        rank.value.num = 10
+        console.log(rank.value.num)
+    }
+</script>
+
+<template>
+    <h1>Rank: {{ rank }}</h1>
+    <button @click="changeRank">Change Rank</button>
+</template>
