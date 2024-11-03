acc3c1924dddb518b481b57e728090a82d64c8da
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-11-03 21:32:29 +0200

    create error

---
+++ 05-NuxtJS Fundamentals [Part 4] (Error Handling)/02- create error/error.vue
@@ -0,0 +1,11 @@
+<script setup>
+    defineProps(['error'])
+</script>
+<template>
+    <div>
+        <h1>Errooorrrrrr 👉👉👉{{ error.statuscode }}</h1>
+        <h1>{{ error.message }}</h1>
+        
+    </div>
+</template>
+
---
+++ 05-NuxtJS Fundamentals [Part 4] (Error Handling)/02- create error/pages/product/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+    const route = useRoute()
+    const id = route.params.id
+    const {data: product} = await useFetch(`https://fakestoreapi.com/products/${id}`)
+    if(!product.value) {
+        throw createError({
+            statusCode: 404,
+            message: 'Page not found'
+        })
+    }
+</script>
+<template>
+    <div>
+        {{ product }}
+    </div>
+</template>
+
