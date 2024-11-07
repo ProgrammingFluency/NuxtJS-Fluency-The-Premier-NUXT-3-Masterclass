0f66ce31ca4a64e817e15a08216445f0a8c19f87
Noor Fakhry <noor.h.fakhry@gmail.com>
2024-11-07 21:21:49 +0200

    the server directory

---
+++ 06-NuxtJS Fundamentals [Part 5] (Nitro Server)/01- The server directory/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const {data} = await useFetch('/api/hello')
+</script>
+
+<template>
+    <div>
+        <h1>Home page</h1>
+        <h1>{{ data.greeting }}</h1>
+        <p>{{ data.body }}</p>
+    </div>
+</template>
