a37f17458f5b02edb780bc351a68cf79ab8f416a
Noor Fakhry <noor.h.fakhry@gmail.com>
2025-01-02 21:54:16 +0200

    Body handling

---
+++ 06-NuxtJS Fundamentals [Part 5] (Nitro Server)/05- Body Handling/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+    let userName
+    if(process.client) {
+      userName = prompt('What is your name?')
+    }
+    const {data: name} = await useFetch('/users/rec-user', {
+        server: false,
+        method: 'post',
+        body: {
+            name: userName
+        }
+    })
+</script>
+<template>
+    <h1 v-if="name">You added the name: {{ name }}</h1>
+</template>
