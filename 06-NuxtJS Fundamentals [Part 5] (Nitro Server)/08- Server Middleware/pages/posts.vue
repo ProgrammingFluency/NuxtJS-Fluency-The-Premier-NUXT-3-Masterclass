f815dfe9e9a6f5bbff0922a5a74d2f4843eb9102
Noor Fakhry <noor.h.fakhry@gmail.com>
2025-01-02 22:37:10 +0200

    Server middleware

---
+++ 06-NuxtJS Fundamentals [Part 5] (Nitro Server)/08- Server Middleware/pages/posts.vue
@@ -0,0 +1,48 @@
+<script setup>
+    const result = ref(null)
+    const getPosts = async () => {
+        const {data} = await useFetch('/api/posts')
+        result.value = data
+    }
+    const addPost = async () => {
+        const {data} = await useFetch('/api/posts', {
+            method: 'post',
+            body: {
+                newPost: {
+                    id: 3,
+                    title: 'This is new post'
+                }
+            }
+        })
+        result.value = data
+    }
+
+
+    const editPost = async () => {
+        const {data} = await useFetch('/api/posts', {
+            method: 'put',
+            body: {
+                title: 'Creating a social media app with Nuxt'
+            }
+        })
+        result.value = data
+    }
+
+
+    const deletePost = async () => {
+        const {data} = await useFetch('/api/posts', {
+            method: 'delete'
+        })
+        result.value = data
+    }
+</script>
+<template>
+    <div>
+        <button @click="getPosts">Get posts</button>
+        <button @click="addPost">Add post</button>
+        <button @click="editPost">Edit post</button>
+        <button @click="deletePost">Delete post</button>
+        <h1></h1>
+        <p>{{ result }}</p>
+    </div>
+</template>
