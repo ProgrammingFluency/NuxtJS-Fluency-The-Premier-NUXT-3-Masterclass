6938ef912802a46eab4c27f1f493203a64c18f62
Noor Fakhry <noor.h.fakhry@gmail.com>
2025-01-03 00:08:18 +0200

    Using Nuxt Test Utilities

--- 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/Counter.test.js
+++ 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/Counter.test.js
@@ -19,3 +19,21 @@ describe('Counter', () => {
         expect(text.text()).toContain('Counter: 2')
     })
 })
+
+// nuxt test utilities.
+import {mountSuspended} from '@nuxt/test-utils/runtime'
+
+
+describe('Counter', () => {
+    it('Increments counter when the user clicks on the button', async () => {
+        const component = await mountSuspended(Counter)
+        const button = component.find('button')
+        const text = component.find('h1')
+
+        expect(text.text()).toContain('Counter: 1')
+
+        await button.trigger('click')
+
+        expect(text.text()).toContain('Counter: 2')
+    })
+})
--- 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/HelloWorld.test.js
+++ 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/HelloWorld.test.js
@@ -9,3 +9,14 @@ describe('HelloWorld', () => {
         expect(component.text()).toContain('Hello World')
     })
 })
+
+// nuxt test utilities.
+import {mountSuspended, toMatchInlineSnapshot} from '@nuxt/test-utils/runtime'
+
+
+describe('HelloWorld', () => {
+    it('displays Hello World', async () => {
+        const component = await mountSuspended(HelloWorld)
+        expect(component.text()).toMatchInlineSnapshot('Hello World')
+    })
+})
