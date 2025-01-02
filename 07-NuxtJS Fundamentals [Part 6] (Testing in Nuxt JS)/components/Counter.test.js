17d63c908751aea943b2b5b367416db9b7c2ce21
Noor Fakhry <noor.h.fakhry@gmail.com>
2025-01-03 00:03:04 +0200

    Using Vue Test Utilities

---
+++ 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/Counter.test.js
@@ -0,0 +1,21 @@
+import {mount} from '@vue/test-utils'
+import Counter from './Counter'
+import {describe, it, expect} from 'vitest'
+
+
+describe('Counter', () => {
+    it('Increments counter when the user clicks on the button', async () => {
+        const component = mount(Counter)
+        const button = component.find('button')
+        const text = component.find('h1')
+
+
+        expect(text.text()).toContain('Counter: 1')
+
+
+        await button.trigger('click')
+
+
+        expect(text.text()).toContain('Counter: 2')
+    })
+})
---
+++ 07-NuxtJS Fundamentals [Part 6] (Testing in Nuxt JS)/components/Counter.vue
@@ -0,0 +1,9 @@
+<script setup>
+    const counter = ref(1)
+</script>
+
+
+<template>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="counter++"></button>
+</template>
