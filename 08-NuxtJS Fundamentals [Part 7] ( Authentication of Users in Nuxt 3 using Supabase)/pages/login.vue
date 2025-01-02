c41cc1bf023dd629107aad9790e0f5d06d976779
Noor Fakhry <noor.h.fakhry@gmail.com>
2025-01-03 00:30:47 +0200

    Allowing users to Login to our Nuxt App using Supabase

---
+++ 08-NuxtJS Fundamentals [Part 7] ( Authentication of Users in Nuxt 3 using Supabase)/pages/login.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <label >Email address</label>
+        <br>
+        <input v-model="email" type="email" />
+        <br>
+        <label >Password</label>
+        <br>
+        <input v-model="password" type="password" />
+        <br>
+        <br>
+        <button @click="logIn">Login</button>
+
+
+        <div v-if="successMsg">
+                <h1> {{ successMsg }} </h1>
+        </div>
+             <!-- Error Message -->
+        <div v-if="errorMsg">
+            <h1>{{ errorMsg }}</h1>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+    const supabase = useSupabaseClient()
+    const email = useState(() => null)
+    const password = useState(() => null)
+    const user = useSupabaseUser()
+    const successMsg = useState(() => false)
+    const errorMsg = useState(() => false)
+
+
+    if(user.value) await navigateTo('/')
+
+
+    const logIn = async () => {
+        const {data, error} = await supabase.auth.signInWithPassword({
+                email: email.value,
+                password: password.value
+            })
+            if(error){
+                successMsg.value = null
+                errorMsg.value = error.message
+                console.log(error)
+                return
+            }
+           
+            errorMsg.value = null
+            successMsg.value = 'Redirecting...'
+            setTimeout(async () => {
+                successMsg.value = null
+                await navigateTo('/')
+            }, 2000)
+           
+    }
+</script>
